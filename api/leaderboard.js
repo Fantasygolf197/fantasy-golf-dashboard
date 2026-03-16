2b7863f78d00a8eaba3f69da79ebebe2dfb19d54
Fantasygolf197 <nosnhoj0333@yahoo.com>
2026-03-15 20:31:21 -0600

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,19 +1,33 @@
-
 export default async function handler(req, res) {
   try {
     const response = await fetch("https://site.api.espn.com/apis/v2/sports/golf/pga/leaderboard");
     const data = await response.json();
 
-    const competitors = data?.events?.[0]?.competitions?.[0]?.competitors || [];
+    let competitors = [];
+
+    if (data?.events?.[0]?.competitions?.[0]?.competitors) {
+      competitors = data.events[0].competitions[0].competitors;
+    } else if (data?.events?.[0]?.competitors) {
+      competitors = data.events[0].competitors;
+    } else if (data?.competitors) {
+      competitors = data.competitors;
+    }
 
     const players = competitors.slice(0, 50).map((p) => ({
-      name: p?.athlete?.displayName || "",
-      pos: p?.status?.position || "",
-      score: p?.score || "",
-      thru: p?.status?.thru || ""
+      name: p?.athlete?.displayName || p?.athlete?.fullName || "",
+      pos: p?.status?.position || p?.position || "",
+      score: p?.score || p?.status?.displayValue || "",
+      thru: p?.status?.thru || p?.status?.displayThru || ""
     }));
 
-    res.status(200).json({ players });
+    res.status(200).json({
+      players,
+      debug: {
+        hasEvents: !!data?.events,
+        hasCompetitions: !!data?.events?.[0]?.competitions,
+        competitorCount: competitors.length
+      }
+    });
 
   } catch (err) {
     res.status(500).json({
