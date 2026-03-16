5d753ff22b2817dcc647f0d3235ef37b2752d1f6
Fantasygolf197 <nosnhoj0333@yahoo.com>
2026-03-15 20:24:05 -0600

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,1 +1,24 @@
 
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://site.api.espn.com/apis/v2/sports/golf/pga/leaderboard");
+    const data = await response.json();
+
+    const competitors = data?.events?.[0]?.competitions?.[0]?.competitors || [];
+
+    const players = competitors.slice(0, 50).map((p) => ({
+      name: p?.athlete?.displayName || "",
+      pos: p?.status?.position || "",
+      score: p?.score || "",
+      thru: p?.status?.thru || ""
+    }));
+
+    res.status(200).json({ players });
+
+  } catch (err) {
+    res.status(500).json({
+      error: "Leaderboard fetch failed",
+      details: err.message
+    });
+  }
+}
